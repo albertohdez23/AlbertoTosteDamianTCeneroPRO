4f8cbd5e53b21ea3815fcda2d46ea03dd93d8209
albertohdez23 <meggaedrock23@gmail.com>
2022-01-28 16:02:10 +0000

    Se genera el proyecto

---
+++ Proyecto/AlbertoTosteDamianTCeneroPRO/src/albertotostedamiantceneropro/AlbertoTosteDamianTCeneroPRO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package albertotostedamiantceneropro;
+
+/**
+ *
+ * @author Alberto Hernández Medina
+ */
+public class AlbertoTosteDamianTCeneroPRO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("un sout hola");
+    }
+    
+}

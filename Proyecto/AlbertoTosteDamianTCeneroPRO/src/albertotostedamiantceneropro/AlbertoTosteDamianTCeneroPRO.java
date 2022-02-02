f0eec2122bb7161f087451f3c0b81ab6858f4875
albertohdez23 <meggaedrock23@gmail.com>
2022-02-02 00:27:23 +0000

    Se añade al proyecto la clase Jugador, con sus correspondientes getter y setter, además de un ArrayList<Integer> que nos guarda el historial de números en la partida. También se crea el main. En este se ha incluido una bienvenida al jugador, con su respectivo menú, un switch con las opciones para Iniciar Partida, ver Historial, Ver Ranking y Salir. Actualmente solo funciona Ver Ranking, este nos muestra el ArrayList con los número usados y nuestro mejor intento. Todos los datos se han simulado y se han introducido a mano debido a la falta de la clase Partida

--- Proyecto/AlbertoTosteDamianTCeneroPRO/src/albertotostedamiantceneropro/AlbertoTosteDamianTCeneroPRO.java
+++ Proyecto/AlbertoTosteDamianTCeneroPRO/src/albertotostedamiantceneropro/AlbertoTosteDamianTCeneroPRO.java
@@ -5,6 +5,8 @@
  */
 package albertotostedamiantceneropro;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alberto Hernández Medina
@@ -15,11 +17,61 @@ public class AlbertoTosteDamianTCeneroPRO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("BIENVENIDO");
+        System.out.println("Introduce tu nombre:");
+        String nick = sc.nextLine();
+        
+        Jugador P1 = new Jugador(nick);
+        P1.setMejorIntento(3);
+        P1.setPartidasJugadas(5);
+        for (int i = 0; i < 11; i++) {
+            P1.historial.add(i);
+        }
+        
+        menu();
+        
+        boolean salir = false;
+        
+        do{
+            System.out.println("Selecciona una Opción:");
+            int opcion = sc.nextInt(); sc.nextLine();
+            
+            switch(opcion){
+                case 1:
+                    System.out.println("Se inicia la partida");
+                    break;
+                case 2:
+                    System.out.println("Historial");
+                    System.out.println(P1.historial);
+                    System.out.print("Tu mejor partida: ");
+                    System.out.println(P1.getMejorIntento() + " intentos");
+                    break;
+                case 3:
+                    System.out.println("Se muestra el ranking");
+                    break;
+                case 0:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
+            
+        }while(salir);
         
         NumAleatorio numSecreto=new NumAleatorio(10, 1);
-        System.out.println(numSecreto.getNum());
     }
     
+    static void menu(){
+        System.out.println("MENU");
+        System.out.println("===================");
+        System.out.println("1.- Iniciar Partida");
+        System.out.println("2.- Ver Historial");
+        System.out.println("3.- Ver Ranking");
+        System.out.println("0.- EXIT");
+    }
 }
 
 

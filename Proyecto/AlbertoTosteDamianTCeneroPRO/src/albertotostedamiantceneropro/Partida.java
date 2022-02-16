5a127f8fe67aeaffdf69d9d2fd44ba03b29b8c5a
JDamianHdezMartin <damian.hdez.martin@gmail.com>
2022-02-16 23:33:24 +0000

    creación de la clase partida con sus respectivos getter y setter, adicionalmente se creó el método introducir dato y se documentó toda la clase

--- Proyecto/AlbertoTosteDamianTCeneroPRO/src/albertotostedamiantceneropro/Partida.java
+++ Proyecto/AlbertoTosteDamianTCeneroPRO/src/albertotostedamiantceneropro/Partida.java
@@ -5,12 +5,16 @@
  */
 package albertotostedamiantceneropro;
 
+import java.util.Scanner;
+
 /**
- *
+ * Esta clase define todos los elementos de la clase partida y la dificultad de 
+ * la misma
  * @author Alejandro Toste
  * @author JDamian
  */
 public class Partida {
+
     NumAleatorio secreto;
     int mayorQue;
     int menorQue;
@@ -19,5 +23,88 @@ public class Partida {
     int fallos;
     int introducido;
     Jugador nick;
-    
+    int dificultad;
+/**
+ * Este contructor define la estructura de la partida
+ * @param nick nombre del jugador
+ * @param dificultad modo de juego seleccionado por el jugador
+ */
+    public Partida(Jugador nick, int dificultad) {
+        this.nick = nick;
+        this.dificultad = dificultad;
+    }
+
+    public NumAleatorio getSecreto() {
+        return secreto;
+    }
+
+    public void setSecreto(NumAleatorio secreto) {
+        this.secreto = secreto;
+    }
+
+    public int getMayorQue() {
+        return mayorQue;
+    }
+
+    public void setMayorQue(int mayorQue) {
+        this.mayorQue = mayorQue;
+    }
+
+    public int getMenorQue() {
+        return menorQue;
+    }
+
+    public void setMenorQue(int menorQue) {
+        this.menorQue = menorQue;
+    }
+
+    public int getFallos() {
+        return fallos;
+    }
+
+    public void setFallos(int fallos) {
+        this.fallos = fallos;
+    }
+
+    public Jugador getNick() {
+        return nick;
+    }
+
+    public void setNick(Jugador nick) {
+        this.nick = nick;
+    }
+
+    public void iniciarPartida(int dificultad) {
+        this.dificultad = dificultad;
+
+        switch (dificultad) {
+
+            case 1:
+                rangoMin = 0;
+                rangoMax = 25;
+                secreto = new NumAleatorio(rangoMin, rangoMax);
+            case 2:
+                rangoMin = 0;
+                rangoMax = 50;
+                secreto = new NumAleatorio(rangoMin, rangoMax);
+            case 3:
+                rangoMin = 0;
+                rangoMax = 100;
+                secreto = new NumAleatorio(rangoMin, rangoMax);
+        }
+    }
+
+    public void introducirDato() {
+        Scanner sc = new Scanner(System.in);
+        if (introducido == secreto.getNum()) {
+            
+        } else if (introducido < secreto.getNum()) {
+            setMayorQue(secreto.getNum());
+            setFallos(fallos + 1);
+        } else {
+            setMenorQue(secreto.getNum());
+            setFallos(fallos + 1);
+        }
+    }
+
 }

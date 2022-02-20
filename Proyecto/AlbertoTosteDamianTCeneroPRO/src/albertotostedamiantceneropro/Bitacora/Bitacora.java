acd84670293f472fab23864c245ad8d39a5e8b22
JDamianHdezMartin <damian.hdez.martin@gmail.com>
2022-02-20 11:34:19 +0000

    Se crea la class Bitacora, así mismo como todos sus métodos, constructor y documentación

---
+++ Proyecto/AlbertoTosteDamianTCeneroPRO/src/albertotostedamiantceneropro/Bitacora/Bitacora.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package albertotostedamiantceneropro.Bitacora;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+/**
+ * Esta clase define la estructura del buscador de proyectos creados
+ * El usuario podrá tanto buscar, borrar y observar los proyectos creados
+ * @author JDamian
+ */
+public class Bitacora {
+    String nombre;
+    TreeMap<String, Proyecto> Proyecto;
+    TreeSet<Proyecto> proyectoFecha;
+
+    public Bitacora(String nombre) {
+        this.nombre = nombre;
+        Proyecto = new TreeMap<>();
+        proyectoFecha = new TreeSet<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public TreeMap<String, Proyecto> getProyecto() {
+        return Proyecto;
+    }
+
+    public void setProyecto(TreeMap<String, Proyecto> Proyecto) {
+        this.Proyecto = Proyecto;
+    }
+
+    public TreeSet<Proyecto> getProyectoFecha() {
+        return proyectoFecha;
+    }
+
+    public void setProyectoFecha(TreeSet<Proyecto> proyectoFecha) {
+        this.proyectoFecha = proyectoFecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Bitacora{" + "nombre=" + nombre +
+               ", Proyecto=" + Proyecto + 
+                ", proyectoFecha=" + proyectoFecha + '}';
+    }
+    
+}

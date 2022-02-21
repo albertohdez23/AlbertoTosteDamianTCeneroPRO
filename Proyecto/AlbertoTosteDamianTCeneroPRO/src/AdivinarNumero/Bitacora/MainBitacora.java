d7a6a7727f9588ee5b1b3ddee0779f559609b91f
al3j4ndr0tr22 <al3j4ndr0tr22@gmail.com>
2022-02-21 17:38:00 +0000

    -Actualizacion de 2022-02-21 17:37:59-

--- Proyecto/AlbertoTosteDamianTCeneroPRO/src/AdivinarNumero/Bitacora/MainBitacora.java
+++ Proyecto/AlbertoTosteDamianTCeneroPRO/src/AdivinarNumero/Bitacora/MainBitacora.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package albertotostedamiantceneropro.Bitacora;
+package AdivinarNumero.Bitacora;
 
 import java.util.Scanner;
 import java.util.TreeSet;
